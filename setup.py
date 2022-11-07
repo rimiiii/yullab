e97fd4e80591540221e922cc59e2eca179cc69ef
waldy <waldy@afoter.com>
2022-11-07 12:26:35 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='yullab',
+    packages=find_packages(),
+    version='0.1',
+    description='Yullab Experiments',
+    author='Yurim',
+    author_email='rimiiii.u@gmail.com',
+    license='MIT',
+    url='https://github.com/rimiiii',
+)
