58b2f3abc598e8d9eb9f8693e4c36aef3b7fd018
Rimiiii <rimiiii@Rimiiiis-MacBook-Air.local>
2022-11-10 00:35:37 +0900

    fix

--- yullab/dataset.py
+++ yullab/dataset.py
@@ -7,8 +7,6 @@ import pandas as pd
 class FashionDataset(Dataset):
     def __init__(self, train_filename):
         self.df = pd.read_csv(train_filename)
-        self.height = 28
-        self.width = 28
 
     def __len__(self):
         return len(self.df)
@@ -16,5 +14,4 @@ class FashionDataset(Dataset):
     def __getitem__(self, idx):
         label = self.df.iloc[idx][0]
         img = np.array(self.df.iloc[idx][1:])
-        img = img.reshape(-1, self.height, self.width)
-        return torch.FloatTensor(img), label
+        return img, label
--- yullab/models.py
+++ yullab/models.py
@@ -7,20 +7,13 @@ class MLP(nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(784, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(256, 10)
         self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
-        x = torch.flatten(x, start_dim=1)
-        x = self.fc1(x)
-        x = F.relu(x)
+        x = F.relu(self.fc1(x))
         x = self.dropout(x)
-        x = self.fc2(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.fc3(x)
-        x = F.log_softmax(x, dim=1)
+        x = F.relu(self.fc2(x))
         return x
 
 
@@ -29,25 +22,41 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3)
         self.conv2 = nn.Conv2d(32, 128, 3)
-        self.conv3 = nn.Conv2d(128, 32, 3)
-        self.fc1 = nn.Linear(32*22*22, 128)
         self.fc2 = nn.Linear(128, 10)
-        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.conv3(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = torch.flatten(x, start_dim=1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
+        x = F.relu(self.conv1(x))
+        x = nn.MaxPool2d(x)
+        x = F.relu(self.conv2(x))
+        x = x.reshape()
+        x = F.relu(self.fc1(x))
+        return x
+
+
+class RNN(nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+        self.rnn1 = nn.RNN(input_size=28, hidden_size=128)
+        self.rnn2 = nn.RNN(input_size=128, hidden_size=64)
+        self.fc1 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        outputs, hidden = self.rnn1(x)
+        outputs, hidden = self.rnn2(hidden)
+        x = self.fc1(outputs[-1])
         x = F.log_softmax(x, dim=1)
+        return x
+
+
+class LSTM(nn.Module):
+    def __init__(self):
+        super(LSTM, self).__init__()
+        self.lstm = nn.LSTM(input_size=28, hidden_size=128, batch_first=True)
+        self.fc1 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        outputs, hidden = self.lstm(x)
+        print(outputs.shape)
+        x = self.fc1(outputs[-1])
+        #x = F.log_softmax(x, dim=1)
         return x
