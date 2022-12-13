f4ed2957a968a3c592eba1aeff3053e8f30275df
waldy <waldy@afoter.com>
2022-12-13 19:00:27 +0900

    music vae

---
+++ yullab/utils.py
@@ -0,0 +1,36 @@
+FULL_DRUM_PITCH_CLASSES = [
+    [p] for p in  # pylint:disable=g-complex-comprehension
+    [36, 35, 38, 27, 28, 31, 32, 33, 34, 37, 39, 40, 56, 65, 66, 75, 85, 42, 44,
+     54, 68, 69, 70, 71, 73, 78, 80, 46, 67, 72, 74, 79, 81, 45, 29, 41, 61, 64,
+     84, 48, 47, 60, 63, 77, 86, 87, 50, 30, 43, 62, 76, 83, 49, 55, 57, 58, 51,
+     52, 53, 59, 82]]
+ROLAND_DRUM_PITCH_CLASSES = [
+    # kick drum
+    [36],
+    # snare drum
+    [38, 37, 40],
+    # closed hi-hat
+    [42, 22, 44],
+    # open hi-hat
+    [46, 26],
+    # low tom
+    [43, 58],
+    # mid tom
+    [47, 45],
+    # high tom
+    [50, 48],
+    # crash cymbal
+    [49, 52, 55, 57],
+    # ride cymbal
+    [51, 53, 59]
+]
+
+def _classes_to_map(classes):
+      class_map = {}
+      for cls, pitches in enumerate(classes):
+        for pitch in pitches:
+          class_map[pitch] = cls
+      return class_map
+
+def class_map():
+    return _classes_to_map(ROLAND_DRUM_PITCH_CLASSES)
